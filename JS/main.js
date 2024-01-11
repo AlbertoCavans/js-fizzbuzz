21984f43eab6fb1f4209d180e0da9a3427d4a611
AlbertoCavans <alberto96cavani@gmail.com>
2024-01-11 16:17:24 +0100

    Feat JS - buzz, fizz & fizzbuzz console.part

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,15 @@
 /* CICLO DI NUMERI */
 for (let i = 1; i <= 100; i++) {
-  console.log(i);
+  /* ###### FIZZBUZZ */
+  if (i % 15 == 0) {
+    console.log("FIZZBUZZ");
+  } else if (i % 5 == 0) {
+    /* ###### BUZZ */
+    console.log("BUZZ");
+  } else if (i % 3 == 0) {
+    /* ###### FIZZ */
+    console.log("FIZZ");
+  } else {
+    console.log(i);
+  }
 }
