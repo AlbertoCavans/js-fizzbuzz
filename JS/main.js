b6d4da69599673977d4258c9a9a122f329793fb1
AlbertoCavans <alberto96cavani@gmail.com>
2024-01-11 17:41:23 +0100

    Fix JS - adding variable word

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,25 @@
-const cardsContainer = document.getElementById("cardsContainer");
-
+/* COLLEGAMENTI DOM */
+const rowCol = document.getElementById("rowCol");
+let cardsNumber = "";
 /* CICLO DI NUMERI */
 for (let i = 1; i <= 100; i++) {
-  cardsContainer.innerHTML += `
-    <div class="card"><h2>${i}</h2></div>`;
+  let word = i;
   /* ###### FIZZBUZZ */
   if (i % 15 == 0) {
-    console.log("FIZZBUZZ");
+    word = "FIZZBUZZ";
   } else if (i % 5 == 0) {
     /* ###### BUZZ */
-    console.log("BUZZ");
+    word = "BUZZ";
   } else if (i % 3 == 0) {
     /* ###### FIZZ */
-    console.log("FIZZ");
-  } else {
-    console.log(i);
+    word = "FIZZ";
   }
+  cardsNumber += `
+  <div class="col-6 col-md-4 col-lg-2">
+  <div class="card my-2 py-5 bg-primary d-flex align-items-center">
+  <span class="h4 text-light">${word}</span>
+  </div>
+  </div>`;
 }
+
+rowCol.innerHTML = cardsNumber;
